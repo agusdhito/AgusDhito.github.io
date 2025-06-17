d75fd0af6931c606f0b6841f3b3d89d7fbd63e57
Agustinus Ardhito <aardhito212@gmail.com>
2025-06-17 11:50:22 +0700

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ResponsiveAppBar from "@/app/ui/NavBar";
 import { Footer } from "@/app/ui/Footer";
-import { Container } from "@mui/material";
 import { Montserrat, Open_Sans } from 'next/font/google';
 
-export const montserrat = Montserrat({
+const montserrat = Montserrat({
   subsets: ['latin'],
   display: 'swap',
   weight: ['400', '500', '600', '700'],
   variable: '--font-montserrat',
 });
 
-export const openSans = Open_Sans({
+const openSans = Open_Sans({
   subsets: ['latin'],
   display: 'swap',
   weight: ['300', '400', '500', '600'],
   variable: '--font-opensans',
 });
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Agustinus Ardhito",
@@ -42,13 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${montserrat.variable} ${openSans.variable}`}>
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        className="antialiased"
-      >
+      <body className="antialiased">
         <ResponsiveAppBar />
         {children}
-
         <Footer />
       </body>
     </html>
